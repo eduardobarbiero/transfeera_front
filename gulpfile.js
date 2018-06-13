dfc8fe4fc105a5f00919ef43b7c58711398398e6
eduardobarbiero <eduardombarbiero@gmail.com>
2018-06-13 15:53:33 -0300

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sh = require('shelljs');
 var replace = require('gulp-replace');
-var template = require('./src/app/components/ekyte/common/ScaffoldGenerator');
 var concat = require('gulp-concat');
-var less = require('gulp-less');
 
 // Altera url de API para Desenvolvimento
 gulp.task('dev', function () {
@@ -28,11 +26,10 @@ gulp.task('production', function () {
 gulp.task('run-app', function (eb) {
     sh.exec("npm run dev", function (code, stdout, stderr) {
         console.log(stdout);
-    })
-    gulp.watch('./src/assets/css/less/**/*.less', ['less']);	    
+    })    
 });
 
-// Executa servidor com base no parametro passado
+// Executa servidor(url) com base no parametro passado
 // utilizar parametro --env
 gulp.task('run', function (eb) {
     sh.exec("gulp " + (gutil.env.env ? gutil.env.env : 'dev'), function (code, stdout, stderr) {
